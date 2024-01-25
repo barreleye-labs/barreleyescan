0790afa658900cca6b3d2a62d585b4ab4e7d0962
usiyoung <usiyoung7@gmail.com>
2024-01-26 02:18:58 +0900

    feat(api): connect the Barreleye API using swr

--- src/pages/blocks/index.tsx
+++ src/pages/blocks/index.tsx
@@ -1,5 +1,20 @@
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import useSWR from 'swr';
+
+import fetcher from '@utils/fetcher';
+
 const Blocks = () => {
-  return <div>blocks</div>;
+  const { data, error } = useSWR('/api/block/1', fetcher);
+  console.log(data, error);
+
+  const columns: GridColDef[] = [
+    { field: 'Height', headerName: 'BLOCK' },
+    { field: 'Timestamp', headerName: 'TIME' },
+    { field: 'TxResponse.TxCount', headerName: 'TOTAL TXS' },
+    { field: 'Hash', headerName: 'HASH' }
+  ];
+
+  return data && <DataGrid rows={[data].map((item, index) => ({ ...item, id: index }))} columns={columns} />;
 };
 
 export default Blocks;
