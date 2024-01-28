b1d16cd514a17b6cb78b9bf421f847c0fa488935
usiyoung <usiyoung7@gmail.com>
2024-01-28 22:47:42 +0900

    feat: set base url and redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
 import DefaultLayout from '@layouts/default';
 
 import './App.css';
 
 export function App() {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    if (pathname === '/') navigate('/dashboard');
+  }, [pathname]);
+
   return (
     <>
       <DefaultLayout />
